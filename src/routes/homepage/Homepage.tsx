28cb81b7729ceb382b1f44e72b30ac1961e4041f
vikrant <you@example.com>
2024-07-26 15:13:30 +0530

    created homepage with type animation

--- src/routes/homepage/Homepage.tsx
+++ src/routes/homepage/Homepage.tsx
@@ -1,20 +1,80 @@
 import { Link } from "react-router-dom";
 import "./homepage.css";
+import { TypeAnimation } from "react-type-animation";
+import { useState } from "react";
+
 const Homepage = () => {
+  const [typingStatus, setTypingStatus] = useState("human1");
+
   return (
     <div className="homepage">
-      {/* <Link to="/dashboard">dashboard</Link> */}
+      <img src="/orbital.png" alt="" className="orbital" />
       <div className="left">
         <h1>Immeasurable AI</h1>
-        <h2>Introducing ChatGPT</h2>
+        <h2>Supercharge your creativity and productivity</h2>
         <h3>
-          We’ve trained a model called ChatGPT which interacts in a
-          conversational way. The dialogue format makes it possible for ChatGPT
-          to answer followup questions, admit its mistakes, challenge incorrect
-          premises, and reject inappropriate requests.
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat sint
+          dolorem doloribus, architecto dolor.
         </h3>
+        <Link to="/dashboard">Get Started</Link>
       </div>
-      <div className="right"></div>
+      <div className="right">
+        <div className="imgContainer">
+          <div className="bgContainer">
+            <div className="bg"></div>
+          </div>
+          <img src="/bot.png" alt="" className="bot" />
+          <div className="chat">
+            <img
+              src={
+                typingStatus === "human1"
+                  ? "/human1.jpeg"
+                  : typingStatus === "human2"
+                  ? "/human2.jpeg"
+                  : "bot.png"
+              }
+              alt=""
+            />
+            <TypeAnimation
+              sequence={[
+                // Same substring at the start will only be typed out once, initially
+                "Human:We produce food for Mice",
+                2000,
+                () => {
+                  setTypingStatus("bot");
+                },
+                "Bot:We produce food for Hamsters",
+                2000,
+                () => {
+                  setTypingStatus("human2");
+                },
+                "Human2:We produce food for Guinea Pigs",
+                2000,
+                () => {
+                  setTypingStatus("bot");
+                },
+                "Bot:We produce food for Chinchillas",
+                2000,
+                () => {
+                  setTypingStatus("human1");
+                },
+              ]}
+              wrapper="span"
+              repeat={Infinity}
+              cursor={true}
+              omitDeletionAnimation={true}
+            />
+          </div>
+        </div>
+      </div>
+      <div className="terms">
+        <img src="/logo.png" alt="" />
+        <div className="links">
+          <Link to="/">Terms of Service</Link>
+          <span>|</span>
+          <Link to="/">Privacy Policy</Link>
+        </div>
+      </div>
     </div>
   );
 };
